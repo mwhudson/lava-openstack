7d89156c930487dc8035de24d70ac9c3f1aec9ec
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-06-25 15:16:12 +1200

    set up lxc to use bridged networking

--- do-packaged-stack.sh
+++ do-packaged-stack.sh
@@ -18,6 +18,20 @@ sudo -u ubuntu ssh ubuntu@controller01 true
 
 if [ `lava-role` = "controller" ]; then
     apt-get install -y juju-core juju-deployer git testrepository subunit python-nose python-lxml python-openstackclient
+    sed -e 's/^USE_LXC_BRIDGE="true"/USE_LXC_BRIDGE="false"/' -i /etc/default/lxc-net
+    service lxc-net restart
+
+    ifdown eth0
+    mv /etc/network/interfaces.d/eth0.cfg /etc/network/interfaces.d/eth0.cfg.bak
+    cat <<-"EOF" > /etc/network/interfaces.d/bridge.cfg
+auto eth0
+iface eth0 inet manual
+
+auto lxcbr0
+iface lxcbr0 inet dhcp
+bridge_ports eth0
+EOF
+    ifup eth0 lxcbr0
     sudo -u ubuntu PATH=$PATH ./do-packaged-stack-controller.sh
 fi
 
