c0a3e89df57593265ae5b0ed781360d1469a3bfa
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-07-10 15:59:45 +1200

    hmm

--- tempest.sh
+++ tempest.sh
@@ -21,8 +21,8 @@ sudo pip install -r requirements.txt
 testr init
 testr list-tests $LAVA_TESTS_TO_RUN | tail -n +6 > /home/ubuntu/all-tests.txt
 testr run --parallel --subunit --load-list=/home/ubuntu/all-tests.txt | tail -n +6 | tee results.subunit | subunit-2to1 | tools/colorizer.py
-cat results.subunit | subunit2csv --no-passthrough > /home/ubuntu/attachments/results.csv
+cat results.subunit | subunit2csv --no-passthrough > /home/ubuntu/results.csv
 
-lava-test-run-attach /home/ubuntu/all-tests.txt
-lava-test-run-attach /home/ubuntu/results.csv
+sudo -E lava-test-run-attach /home/ubuntu/all-tests.txt
+sudo -E lava-test-run-attach /home/ubuntu/results.csv
 python $mydir/simplify-results.py /home/ubuntu/results.csv  /home/ubuntu/all-tests.txt
