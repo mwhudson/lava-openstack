bb130b59b80208235b517c4747b67be73b0a91bd
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-07-18 11:37:30 +1200

    respect LAVA_RUN_TEMPEST

--- tempest.sh
+++ tempest.sh
@@ -16,15 +16,17 @@ controller_address=$(unitAddress keystone 0)
 sed -e "s/@IMAGE_UUID@/$IMAGE_UUID/g" -e "s/@CONTROLLER_IP@/$controller_address/g" \
     -e "s/@SECRET@/$secret/g" -e "s/@ACCESS@/$access/g" \
     $mydir/tempest.conf.in > ~/tempest/etc/tempest.conf
-cd ~/tempest
-sudo pip install -r requirements.txt
-testr init
-testr list-tests $LAVA_TESTS_TO_RUN | tail -n +6 > /home/ubuntu/all-tests.txt
-testr run --parallel --subunit --load-list=/home/ubuntu/all-tests.txt | tail -n +6 | tee results.subunit | subunit-2to1 | tools/colorizer.py
-sudo apt-get install -y subunit
-cat results.subunit | subunit2csv --no-passthrough > /home/ubuntu/results.csv
+if [ "$LAVA_RUN_TEMPEST" = "yes" ]; then
+    cd ~/tempest
+    sudo pip install -r requirements.txt
+    testr init
+    testr list-tests $LAVA_TESTS_TO_RUN | tail -n +6 > /home/ubuntu/all-tests.txt
+    testr run --parallel --subunit --load-list=/home/ubuntu/all-tests.txt | tail -n +6 | tee results.subunit | subunit-2to1 | tools/colorizer.py
+    sudo apt-get install -y subunit
+    cat results.subunit | subunit2csv --no-passthrough > /home/ubuntu/results.csv
 
-echo $PATH
-sudo -E lava-test-run-attach /home/ubuntu/all-tests.txt
-sudo -E lava-test-run-attach /home/ubuntu/results.csv
-python $mydir/simplify-results.py /home/ubuntu/results.csv  /home/ubuntu/all-tests.txt
+    echo $PATH
+    sudo -E lava-test-run-attach /home/ubuntu/all-tests.txt
+    sudo -E lava-test-run-attach /home/ubuntu/results.csv
+    python $mydir/simplify-results.py /home/ubuntu/results.csv  /home/ubuntu/all-tests.txt
+fi
