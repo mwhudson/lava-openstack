f98253cc66fa19b98205581793122e05be186676
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-07-24 12:20:54 +1200

    run tempest in a container when installing with juju

--- install-tempest.sh
+++ install-tempest.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 mydir=$(dirname $(readlink -f $0))
-sudo apt-get install -y python-pip
-git clone https://github.com/openstack/tempest.git ~/tempest
+
 . ~/ubuntu-openrc
 IMAGE_UUID=`glance image-list | awk '/linaro.*ami/{print $2}'`
 access=$(keystone ec2-credentials-create | grep access | awk '{ print $4 }')
@@ -11,14 +10,28 @@ unitAddress()
 {
 	juju status $1 | python -c "import yaml; import sys; print yaml.load(sys.stdin)[\"services\"][\"$1\"][\"units\"][\"$1/$2\"][\"public-address\"]" 2> /dev/null
 }
+
 controller_address=$(unitAddress keystone 0)
 
 sed -e "s/@IMAGE_UUID@/$IMAGE_UUID/g" -e "s/@CONTROLLER_IP@/$controller_address/g" \
     -e "s/@SECRET@/$secret/g" -e "s/@ACCESS@/$access/g" \
-    $mydir/tempest.conf.in > ~/tempest/etc/tempest.conf
-sudo -E PATH=$PATH lava-test-run-attach ~/tempest/etc/tempest.conf
+    $mydir/tempest.conf.in > /tmp/tempest.conf
+host=$($mydir/add-new-lxc.py)
+
+cat > /tmp/install.sh <<EOF
+sudo apt-get install -y bridge-utils pylint python-setuptools screen unzip wget psmisc git openssh-server openssl python-virtualenv python-unittest2 iputils-ping wget curl tcpdump euca2ools tar python-dev python2.7 bc libxslt1-dev zlib1g-dev 
+git clone https://github.com/openstack/tempest.git ~/tempest
 cd ~/tempest
 sudo pip install -r requirements.txt
-# ... aaaand put the files back from apt
-sudo apt-get install -y --reinstall python-glanceclient python-keystoneclient python-swiftclient
 testr init
+EOF
+juju scp /tmp/install.sh $host:
+juju ssh $host sh -x install.sh
+juju scp /tmp/tempest.conf $host:tempest/etc/
+cat > ~/run-in-tempest-dir.sh <<EOF
+#!/bin/sh
+juju scp \$1 $host:script.sh
+juju ssh $host 'cd tempest && ~/script.sh'
+juju scp -r $host:output ~/output
+EOF
+chmod u+x ~/run-in-tempest-dir.sh
--- tempest.sh
+++ tempest.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -x
 mydir=$(dirname $(readlink -f $0))
-cd ~/tempest
-sudo pip install -r requirements.txt
 testr init
+mkdir ~/output
+cp etc/tempest.conf ~/output
 if [ "$LAVA_RUN_TEMPEST" = "yes" ]; then
-    testr list-tests $LAVA_TESTS_TO_RUN | tail -n +6 > /home/ubuntu/all-tests.txt
-    OS_TEST_TIMEOUT=1200 testr run --subunit --load-list=/home/ubuntu/all-tests.txt | tail -n +6 | tee results.subunit | subunit-2to1 | tools/colorizer.py
+    testr list-tests $LAVA_TESTS_TO_RUN | tail -n +6 > /home/ubuntu/output/all-tests.txt
+    OS_TEST_TIMEOUT=1200 testr run --subunit --load-list=/home/ubuntu/output/all-tests.txt | tail -n +6 | tee results.subunit | subunit-2to1 | tools/colorizer.py
     sudo apt-get install -y subunit
-    cat results.subunit | subunit2csv --no-passthrough > /home/ubuntu/results.csv
+    cat results.subunit | subunit2csv --no-passthrough > /home/ubuntu/output/results.csv
 
-    sudo -E PATH=$PATH lava-test-run-attach /home/ubuntu/all-tests.txt
-    sudo -E PATH=$PATH lava-test-run-attach /home/ubuntu/results.csv
-    python $mydir/simplify-results.py /home/ubuntu/results.csv  /home/ubuntu/all-tests.txt
 fi
