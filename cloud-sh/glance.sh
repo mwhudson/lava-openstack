57fd4cb92a6d0f78b6016e0d697c4b3d37a79b30
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-10-13 14:01:39 +1300

    lets try linaro-minimal again

--- cloud-sh/glance.sh
+++ cloud-sh/glance.sh
@@ -6,8 +6,8 @@ cd "$(mktemp -d)"
 
 IMAGE_NAME=linaro-minimal
 IMAGE_URL=http://people.linaro.org/~mwhudson/linaro-minimal-arm64-uec-psci.tar.gz
-IMAGE_NAME=trusty-server
-IMAGE_URL=http://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-`dpkg --print-architecture`.tar.gz
+#IMAGE_NAME=trusty-server
+#IMAGE_URL=http://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-`dpkg --print-architecture`.tar.gz
 
 wget --progress=dot -e dotbytes=10M -O uec.tar.gz $IMAGE_URL
 
@@ -22,10 +22,20 @@ IMG_PROPERTY=" --property hw_machine_type=virt  --property hw_cdrom_bus=virtio"
 
 sudo mount-image-callback --verbose $IMAGE -- sh -xc 'cp -v $MOUNTPOINT/boot/initrd* . && chmod ugo+r initrd*'
 
-RAMDISK_ID=$(glance image-create --name "$IMAGE_NAME-ramdisk" --is-public True --container-format ari --disk-format ari < initrd* | grep ' id ' |  awk -F'[ \t]*\\|[ \t]*' '{ print $3 }')
+if [ -e initrd* ]; then
+    RAMDISK_ID=$(glance image-create --name "$IMAGE_NAME-ramdisk" --is-public True --container-format ari --disk-format ari < initrd* | grep ' id ' |  awk -F'[ \t]*\\|[ \t]*' '{ print $3 }')
+    IMG_PROPERTY="$IMG_PROPERTY --property ramdisk_id=$RAMDISK_ID"
+    COMMANDLINE="root=LABEL=cloudimg-rootfs rw rootwait console=ttyAMA0"
+else
+    COMMANDLINE=""
+fi
 
 KERNEL_ID=$(glance image-create --name "$IMAGE_NAME-kernel" --is-public True --container-format aki --disk-format aki < "$KERNEL" | grep ' id ' | awk -F'[ \t]*\\|[ \t]*' '{ print $3 }')
 
-IMAGE_UUID=$(glance image-create --name "${IMAGE_NAME}" $IMG_PROPERTY --is-public True --container-format ami --disk-format ami --property kernel_id=$KERNEL_ID --property ramdisk_id=$RAMDISK_ID < "${IMAGE}" | grep ' id ' | awk -F'[ \t]*\\|[ \t]*' '{ print $3 }')
+IMG_PROPERTY="$IMG_PROPERTY --property kernel_id=$KERNEL_ID"
 
-glance image-update $IMAGE_UUID --property os_command_line='root=LABEL=cloudimg-rootfs rw rootwait console=ttyAMA0'
+IMAGE_UUID=$(glance image-create --name "${IMAGE_NAME}" $IMG_PROPERTY --is-public True --container-format ami --disk-format ami < "${IMAGE}" | grep ' id ' | awk -F'[ \t]*\\|[ \t]*' '{ print $3 }')
+
+if [ -n "$COMMANDLINE" ]; then
+    glance image-update $IMAGE_UUID --property os_command_line="$COMMANDLINE"
+fi
