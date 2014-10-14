bb63d643c5ced687fb540bfd969402fec7a855e3
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-10-14 15:24:35 +1300

    mime types

--- tempest.sh
+++ tempest.sh
@@ -3,7 +3,7 @@ if [ ! -d .testrepository ]; then
     testr init
 fi
 mkdir ~/output
-cp etc/tempest.conf ~/output
+cp etc/tempest.conf ~/output/tempest_conf.txt
 if [ "$LAVA_RUN_TEMPEST" = "yes" ]; then
     testr list-tests $LAVA_TESTS_TO_RUN | tail -n +6 > /home/ubuntu/output/all-tests.txt
     OS_TEST_TIMEOUT=1200 testr run --subunit --load-list=/home/ubuntu/output/all-tests.txt | tail -n +6 | tee results.subunit | subunit-2to1 | tools/colorizer.py
