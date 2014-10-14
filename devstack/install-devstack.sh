4abb980ef92bd90b3e5273b02903e414fff5dc52
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-10-15 12:00:40 +1300

    kill one script

--- devstack/install-devstack.sh
+++ devstack/install-devstack.sh
@@ -1,19 +1,39 @@
 #!/bin/bash -xe
-apt-get update
-apt-get -y install lsb-release git nmap vim pm-utils bridge-utils openssh-server subunit
 
-cp ./devstack/tempest-run-in-tempest-dir.sh /home/ubuntu/run-in-tempest-dir.sh
+mydir=$(dirname $(readlink -f $0))
+
+apt-get update
+apt-get -y install lsb-release git nmap vim pm-utils bridge-utils openssh-server subunit qemu-system libvirt-bin python-libvirt ntpdate
+
+cp $mydir/tempest-run-in-tempest-dir.sh /home/ubuntu/run-in-tempest-dir.sh
 
 d=$(mktemp -d)
 chmod -R a+rX $d
 cd $d
 
+git clone git://github.com/mwhudson/devstack.git -b arm64-trusty-icehouse
+
+cp $mydir/local.sh ./devstack
+cp $mydir/local.conf ./devstack
+
+sudo modprobe nbd
+
+chown -R ubuntu:ubuntu ./devstack
+cd ./devstack
+export DEVSTACK_ROOT=`pwd`
 exec 1>/dev/tty
 exec 2>/dev/tty
-./devstack/run-devstack.sh
+su --login --shell "/bin/bash" ubuntu <<EOF
+export PATH=/sbin:/usr/sbin:$PATH
+env
+cd ${DEVSTACK_ROOT}
+./stack.sh
+EOF
 
 sleep 60
 if [ "$LAVA_SLEEP_FOR_ACCESS" = "yes" ]; then
     sleep $LAVA_SLEEP_DURATION
 fi
+
+sudo ntpdate ntp.ubuntu.com
 
