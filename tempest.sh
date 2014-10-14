93df468da52acf86ba811ae77a78e6580bd34511
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-10-14 15:47:00 +1300

    run tests in parallel

--- tempest.sh
+++ tempest.sh
@@ -6,7 +6,7 @@ mkdir ~/output
 cp etc/tempest.conf ~/output/tempest_conf.txt
 if [ "$LAVA_RUN_TEMPEST" = "yes" ]; then
     testr list-tests $LAVA_TESTS_TO_RUN | tail -n +6 > /home/ubuntu/output/all-tests.txt
-    OS_TEST_TIMEOUT=1200 testr run --subunit --load-list=/home/ubuntu/output/all-tests.txt | tail -n +6 | tee results.subunit | subunit-2to1 | tools/colorizer.py
+    OS_TEST_TIMEOUT=1200 testr run --parallel --concurrency=4 --subunit --load-list=/home/ubuntu/output/all-tests.txt | tail -n +6 | tee results.subunit | subunit-2to1 | tools/colorizer.py
     sudo apt-get update
     sudo apt-get install -y subunit
     cat results.subunit | subunit2csv --no-passthrough > /home/ubuntu/output/results.csv
