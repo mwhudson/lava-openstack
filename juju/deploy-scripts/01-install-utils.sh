354e4d3155f088b39e79decd64d510f855c5c392
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-10-15 15:14:56 +1300

    grr

--- juju/deploy-scripts/01-install-utils.sh
+++ juju/deploy-scripts/01-install-utils.sh
@@ -1,6 +1,6 @@
 # Just some stuff to make later steps easier to read.
 
-sudo apt-get install -y python-openstackclient python-virtualenv libyaml-dev
+sudo apt-get install -y python-openstackclient python-virtualenv libyaml-dev python2.7-dev
 sudo virtualenv /usr/local/venv
 sudo /usr/local/venv/bin/pip install shyaml
 sudo ln -s /usr/local/venv/bin/shyaml /usr/local/bin
