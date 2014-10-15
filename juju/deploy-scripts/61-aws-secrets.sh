31ad5b5739f61d733dac636df744dd121b3acb73
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-10-16 08:08:08 +1300

    i love openstack and its consistent command line ui

--- juju/deploy-scripts/61-aws-secrets.sh
+++ juju/deploy-scripts/61-aws-secrets.sh
@@ -1,4 +1,5 @@
 # Create ec2 credentials for tempest to use.
 . ~/ubuntu-openrc
-add-subst ACCESS $(keystone ec2-credentials-create | grep access | awk '{ print $4 }')
-add-subst SECRET $(keystone ec2-credentials-get --access $access | grep secret | awk '{ print $4 }')
+ACCESS=$(keystone ec2-credentials-create | grep access | awk '{ print $4 }')
+add-subst ACCESS $ACCESS
+add-subst SECRET $(keystone ec2-credentials-get --access $ACCESS | grep secret | awk '{ print $4 }')
