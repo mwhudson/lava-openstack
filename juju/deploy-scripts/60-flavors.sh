b795d794f48f22abe3956317bb36b02a07918487
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-10-16 09:01:37 +1300

    aaaaa

--- juju/deploy-scripts/60-flavors.sh
+++ juju/deploy-scripts/60-flavors.sh
@@ -1,5 +1,5 @@
 # Create flavors for tempest to use.  It's important that these
 # flavors specify 0 as the disk size; that lets the resize tests work.
 . ~/admin-openrc
-add-subst FLAVOR $(nova flavor-create m1.nano auto 128 0 1 | | awk 'NR==4 { print $2 }')
+add-subst FLAVOR $(nova flavor-create m1.nano auto 128 0 1 | awk 'NR==4 { print $2 }')
 add-subst FLAVOR_ALT $(nova flavor-create m1.micro 84 192 0 1 | awk 'NR==4 { print $2 }')
