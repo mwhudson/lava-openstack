be481692c5e832018e84edd574d3c289c3109b70
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-10-15 15:28:07 +1300

    ah haha oops

--- juju/deploy-openstack.sh
+++ juju/deploy-openstack.sh
@@ -6,9 +6,9 @@ cd $(dirname $(readlink -f $0))
 # (in alpabetical order) to set up the openstack deployed by
 # deploy-services.sh in readiness for running tempest.
 
-for script in "deploy-scripts/*.sh"; do
+for script in deploy-scripts/*.sh; do
     echo "executing ${script} ..."
-    bash -ex ${script}
+    bash -ex "${script}"
 done
 
 # import key pair
