a31a445712d976a6e07610800405773c27255951
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-10-15 15:00:09 +1300

    install libyaml before trying to install shyaml

--- juju/deploy-scripts/01-install-utils.sh
+++ juju/deploy-scripts/01-install-utils.sh
@@ -1,7 +1,7 @@
 # Just some stuff to make later steps easier to read.
 
-sudo apt-get install -y python-openstackclient python-virtualenv
-sudo cp utils/* /usr/local/bin
+sudo apt-get install -y python-openstackclient python-virtualenv libyaml-dev
 sudo virtualenv /usr/local/venv
 sudo /usr/local/venv/bin/pip install shyaml
 sudo ln -s /usr/local/venv/bin/shyaml /usr/local/bin
+sudo cp utils/* /usr/local/bin
