7d3cd4702575930ef6e23f0163ff824c8f09fbc8
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-10-17 10:00:37 +1300

    poo

--- tempest/tempest-inner.sh
+++ tempest/tempest-inner.sh
@@ -10,7 +10,7 @@ mkdir ~/output
 cp etc/tempest.conf ~/output/tempest_conf.txt
 if [ "$LAVA_RUN_TEMPEST" = "yes" ]; then
     testr list-tests $LAVA_TESTS_TO_RUN | tail -n +6 > /home/ubuntu/output/all-tests.txt
-    OS_TEST_TIMEOUT=1200 testr run --parallel --concurrency=4 --subunit --load-list=/home/ubuntu/output/all-tests.txt | tail -n +6 > tee results.subunit
+    OS_TEST_TIMEOUT=1200 testr run --parallel --concurrency=4 --subunit --load-list=/home/ubuntu/output/all-tests.txt | tail -n +6 > results.subunit
     sudo apt-get update
     sudo apt-get install -y subunit
     cat results.subunit | subunit2csv --no-passthrough > /home/ubuntu/output/results.csv
